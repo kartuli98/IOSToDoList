bdb2ef7bcb064af5385d7eeb37bc52c187db52f9
viktor mudzhyri <vituha98@gmail.com>
2023-05-03 13:11:58 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Viktor Mudzhyri on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
